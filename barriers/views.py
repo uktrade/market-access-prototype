cb22adaf91a160dd7a570f16f847f73387574d7a
bquinn <brendan@cluefulmedia.com>
2017-10-03 18:09:44 +0100

    Fixed countries lookups, improved session variable support and API call

--- barriers/views.py
+++ barriers/views.py
@@ -5,7 +5,9 @@ import requests
 
 from django.conf import settings
 from django.shortcuts import HttpResponse
-from django.views.generic import TemplateView, FormView, ListView, DetailView
+from django.views.generic import (
+    View, TemplateView, FormView, ListView, DetailView
+)
 from django.urls import reverse_lazy
 
 from api_client import api_client
@@ -13,7 +15,7 @@ from .helpers import (
     store_companies_house_profile_in_session_and_validate,
     has_company
 )
-from .models import BarrierRecord, BarrierSource
+from .models import BarrierRecord, BarrierSource, BarrierCountry
 from .forms import BarrierCountryForm, ReportBarrierForm
 
 from sso.utils import SSOSignUpRequiredMixin
@@ -65,19 +67,20 @@ class ReportBarrierExistingView(FormView):
 
 class ReportBarrierShowCurrentBarriersView(ListView):
     template_name = 'report-barrier-show-current-barriers.html'
-    countries_affected = ''
+    country = ''
     model = BarrierRecord
 
     def dispatch(self, request, *args, **kwargs):
-        self.countries_affected = kwargs['countries']
+        self.country = kwargs['countries']
         return super(ReportBarrierShowCurrentBarriersView, self).dispatch(*args, **kwargs)
 
     def get_queryset(self, *args, **kwargs):
-        return BarrierRecord.objects.filter(notifying_member=self.countries_affected)
+        db_country = BarrierCountry.objects.get(name=self.country)
+        return BarrierRecord.objects.filter(country=db_country)
 
     def get_context_data(self, **kwargs):
         context_data =  super(ReportBarrierShowCurrentBarriersView, self).get_context_data(**kwargs)
-        context_data['countries_affected'] = self.countries_affected
+        context_data['countries_affected'] = self.db_country.name
         return context_data
 
 
@@ -104,110 +107,119 @@ class BarriersByCountryView(ListView):
     model = BarrierRecord
 
     def dispatch(self, request, *args, **kwargs):
-        self.country = kwargs['country']
+        self.country_text = kwargs['country']
         return super(BarriersByCountryView, self).dispatch(request, *args, **kwargs)
 
     def get_queryset(self, *args, **kwargs):
         uk_source = BarrierSource.objects.get(short_name='UK')
-        return BarrierRecord.objects.filter(notifying_member=self.country).filter(barrier_source=uk_source)
+        self.country_object = BarrierCountry.objects.get(name=self.country_text)
+        return BarrierRecord.objects.filter(country=self.country_object).filter(barrier_source=uk_source)
 
     def get_context_data(self, **kwargs):
         context_data =  super(BarriersByCountryView, self).get_context_data(**kwargs)
-        context_data['country'] = self.country
+        context_data['country'] = self.country_text
         # "objects" will be UK barriers, make "wto_barriers"
         # and "ec_barriers" contain the appropriate ones
         wto_source = BarrierSource.objects.get(short_name='WTO')
         ec_source = BarrierSource.objects.get(short_name='EC MADB')
-        context_data['wto_barriers'] = BarrierRecord.objects.filter(notifying_member=self.country).filter(barrier_source=wto_source)
-        context_data['ec_barriers'] = BarrierRecord.objects.filter(notifying_member=self.country).filter(barrier_source=ec_source)
+        context_data['wto_barriers'] = BarrierRecord.objects.filter(country=self.country_object).filter(barrier_source=wto_source)
+        context_data['ec_barriers'] = BarrierRecord.objects.filter(country=self.country_object).filter(barrier_source=ec_source)
         return context_data
 
-class PostGetTemplateView(TemplateView):
-    def post(self, *args, **kwargs):
-        if 'dit[step1][type]' in self.request.POST:
-            if self.request.POST['dit[step1][type]'] == 'I work for a trade association':
-                self.request.session['is_trade_association'] = True
-            else:
-                # Allow it to be turned off again
-                self.request.session['is_trade_association'] = False
-        return self.get(*args, **kwargs)
-
-    def get_context_data(self, *args, **kwargs):
-        context = super(PostGetTemplateView, self).get_context_data(*args, **kwargs)
-        if 'is_trade_association' in self.request.session:
-            context['is_trade_association'] = self.request.session['is_trade_association']
-        return context
 
 class BarrierDetailView(DetailView):
     model = BarrierRecord
     template_name = 'barrier-detail.html'
 
+
+class SessionContextMixin(object):
+    def get_context_data(self, *args, **kwargs):
+        context = super(SessionContextMixin, self).get_context_data(*args, **kwargs)
+        if 'existing' in self.request.session:
+            context['existing'] = self.request.session['existing']
+        if 'is_trade_association' in self.request.session:
+            context['is_trade_association'] = self.request.session['is_trade_association']
+        return context
+
+
 class BarrierDetailStaticView(TemplateView):
     model = BarrierRecord
     template_name = 'barrier-detail-static.html'
 
-class BarrierExtraDetailView(TemplateView):
+class BarrierExtraDetailView(SessionContextMixin, TemplateView):
     model = BarrierRecord
     template_name = 'barrier-extra-detail.html'
 
-class BarrierSubscribeView(TemplateView):
+class BarrierSubscribeView(SessionContextMixin, TemplateView):
     model = BarrierRecord
     template_name = 'barrier-subscribe.html'
 
-class BarriersGeneralInfoView(TemplateView):
+class BarriersGeneralInfoView(SessionContextMixin, TemplateView):
     model = BarrierRecord
     template_name = 'barriers-general-info.html'
 
-class BarriersCaseStudyView(TemplateView):
+class BarriersCaseStudyView(SessionContextMixin, TemplateView):
     model = BarrierRecord
     template_name = 'barriers-case-study.html'
 
-class ReportBarrierStep1View(PostGetTemplateView):
+class ReportBarrierStep1View(SessionContextMixin, TemplateView):
     model = BarrierRecord
     template_name = 'report-barrier-step1.html'
 
-class ReportBarrierStep2View(PostGetTemplateView):
+    def get(self, request, *args, **kwargs):
+        if 'existing' in request.GET:
+            request.session['existing'] = request.GET['existing']
+        return super(ReportBarrierStep1View, self).get(request, *args, **kwargs)
+
+
+class ReportBarrierStep2View(SessionContextMixin, TemplateView):
     model = BarrierRecord
     template_name = 'report-barrier-step2.html'
 
-class ReportBarrierStep3View(PostGetTemplateView):
+    def post(self, request, *args, **kwargs):
+        if 'dit[step1][type]' in self.request.POST:
+            if request.POST['dit[step1][type]'] == 'I work for a trade association':
+                request.session['is_trade_association'] = True
+            else:
+                # Allow it to be turned off again
+                request.session['is_trade_association'] = False
+        return self.get(request, *args, **kwargs)
+
+
+class ReportBarrierStep3View(SessionContextMixin, TemplateView):
     model = BarrierRecord
     template_name = 'report-barrier-step3.html'
 
-class ReportBarrierStep4View(PostGetTemplateView):
+class ReportBarrierStep4View(SessionContextMixin, TemplateView):
     model = BarrierRecord
     template_name = 'report-barrier-step4.html'
 
-class ReportBarrierStep5View(PostGetTemplateView):
+class ReportBarrierStep5View(SessionContextMixin, TemplateView):
     model = BarrierRecord
     template_name = 'report-barrier-step5.html'
 
-class ReportBarrierStep6View(PostGetTemplateView):
+class ReportBarrierStep6View(SessionContextMixin, TemplateView):
     model = BarrierRecord
     template_name = 'report-barrier-step6.html'
 
-class ReportBarrierRegisterView(PostGetTemplateView):
+class ReportBarrierRegisterView(SessionContextMixin, TemplateView):
     model = BarrierRecord
     template_name = 'report-barrier-register.html'
 
-class RequestFastTrackView(TemplateView):
+class RequestFastTrackView(SessionContextMixin, TemplateView):
     model = BarrierRecord
     template_name = 'request-fast-track.html'
 
-class ExampleSummaryView(TemplateView):
+class ExampleSummaryView(SessionContextMixin, TemplateView):
     model = BarrierRecord
     template_name = 'example-summary.html'
 
 
-class CompaniesHouseRequestView(TemplateView):
-    template_name = 'companies-house-api.html'
+class CompaniesHouseRequestView(View):
     search_company = ''
 
-    def dispatch(self, request, *args, **kwargs):
+    def get(self, request, *args, **kwargs):
         self.search_company = request.GET.get('company', '')
-        return super(CompaniesHouseRequestView, self).dispatch(request, *args, **kwargs)
-
-    def render_to_response(self, context, **kwargs):
         api_response = requests.get('https://api.companieshouse.gov.uk/search/companies'
                          '?q={}'
                          .format(self.search_company),
