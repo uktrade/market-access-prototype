4d8c7e210d4fda08be5855b5f3f0585381a8fda4
bquinn <brendan@cluefulmedia.com>
2017-10-04 11:22:04 +0100

    Changed size of URL field because postgres actually cares about such things

--- barriers/models.py
+++ barriers/models.py
@@ -303,7 +303,7 @@ class BarrierSource(AuditableModel):
     name = models.CharField(max_length=100)
     description = models.CharField(max_length=500, null=True, blank=True)
     short_name = models.CharField(max_length=20, null=True, blank=True)
-    remote_url = models.URLField(max_length=20, blank=True, null=True)
+    remote_url = models.URLField(max_length=300, blank=True, null=True)
 
     def __str__(self):
         return self.short_name
