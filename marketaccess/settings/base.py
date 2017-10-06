dd4bb04ce6bd9341226f2921421ffdae20f755af
bquinn <brendan@cluefulmedia.com>
2017-10-06 13:20:44 +0100

    Removing a boring comment

--- marketaccess/settings/base.py
+++ marketaccess/settings/base.py
@@ -15,10 +15,6 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
-
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'tcy4z8^vl8)3!zne519s^!#+69l+_=_p#y84*s@+jl@ewvrylo'
 
